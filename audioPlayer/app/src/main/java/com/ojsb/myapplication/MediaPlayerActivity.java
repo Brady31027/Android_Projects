0cb693697fa4789752fc4074679de26d56aa7650
Brady Cheng <brady31027@gmail.com>
2017-04-26 22:49:45 -0700

    add MediaPlayer

--- audioPlayer/app/src/main/java/com/ojsb/myapplication/MediaPlayerActivity.java
+++ audioPlayer/app/src/main/java/com/ojsb/myapplication/MediaPlayerActivity.java
@@ -1,6 +1,7 @@
 package com.ojsb.myapplication;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,10 +9,16 @@ import android.widget.TextView;
 
 public class MediaPlayerActivity extends Activity {
 
+    // TODO: add streaming and more file format
+    // TODO: add progress showing
+    // TODO: add seeking
+    
     private TextView mTextState;
     private Button mBtnPlay;
     private Button mBtnStop;
     private Button mBtnPause;
+    private MediaPlayerState mCurState;
+    private MediaPlayer player;
 
     private void updateState(MediaPlayerState state) {
         String stateMsg = "State : IDLE";
@@ -66,10 +73,13 @@ public class MediaPlayerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
         bindUi();
+        setupPlayer();
 
         mBtnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                player.start();
+                mCurState = MediaPlayerState.STATE_STARTED;
                 updateState(MediaPlayerState.STATE_STARTED);
             }
         });
@@ -77,14 +87,27 @@ public class MediaPlayerActivity extends Activity {
         mBtnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateState(MediaPlayerState.STATE_PAUSED);
+                if (mCurState == MediaPlayerState.STATE_STARTED) {
+                    player.pause();
+                    mCurState = MediaPlayerState.STATE_PAUSED;
+                    updateState(MediaPlayerState.STATE_PAUSED);
+                }
             }
         });
 
         mBtnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                player.stop();
+                mCurState = MediaPlayerState.STATE_STOPPED;
                 updateState(MediaPlayerState.STATE_STOPPED);
+
+                // delete the current player
+                player.release();
+                player = null;
+
+                // create a new player
+                player = MediaPlayer.create(MediaPlayerActivity.this, R.raw.byte_music);
             }
         });
     }
@@ -94,5 +117,11 @@ public class MediaPlayerActivity extends Activity {
         mBtnPause = (Button) findViewById(R.id.btnPause);
         mBtnPlay = (Button) findViewById(R.id.btnPlay);
         mBtnStop = (Button) findViewById(R.id.btnStop);
+    }
+
+    private void setupPlayer() {
+        // local file, use create()
+        player = MediaPlayer.create(MediaPlayerActivity.this, R.raw.byte_music);
+        updateState(MediaPlayerState.STATE_PREPARED);
     }
 }
