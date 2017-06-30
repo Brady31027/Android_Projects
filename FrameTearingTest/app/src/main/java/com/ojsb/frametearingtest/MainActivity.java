7a8d147e182479683df18ada82554aafe65c793d
Brady Cheng <brady31027@gmail.com>
2017-06-30 11:19:30 +0800

    add click function

--- FrameTearingTest/app/src/main/java/com/ojsb/frametearingtest/MainActivity.java
+++ FrameTearingTest/app/src/main/java/com/ojsb/frametearingtest/MainActivity.java
@@ -1,11 +1,17 @@
 package com.ojsb.frametearingtest;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+
+    private LinearLayout mRootLayout;
+    static boolean mBlackBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,19 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
+        mRootLayout = (LinearLayout) findViewById(R.id.root_layout);
+        mBlackBackground = true;
 
+        mRootLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBlackBackground) {
+                    mRootLayout.setBackgroundColor(Color.WHITE);
+                } else {
+                    mRootLayout.setBackgroundColor(Color.BLACK);
+                }
+                mBlackBackground = !mBlackBackground;
+            }
+        });
     }
 }
